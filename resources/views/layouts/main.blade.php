a9ef48f4a89df1ad456f993650da97a77384064a
Nazirul Fitri <nazirul@csc.net.my>
2024-03-09 22:41:22 +0800

    Mobile sidebar & navbar

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,16 +2,18 @@
 
 @section('body')
 <div>
-    @include('include.sidebar')
+    <div x-data="{ open: false }" @keydown.window.escape="open = false">
+        @include('include.sidebar')
 
-    <div class="lg:pl-72">
-        @include('include.navbar')
+        <div class="lg:pl-72">
+            @include('include.navbar')
 
-        <main class="py-10">
-            <div class="px-4 sm:px-6 lg:px-8">
-                @yield('content')
-            </div>
-        </main>
+            <main class="py-10">
+                <div class="px-4 sm:px-6 lg:px-8">
+                    @yield('content')
+                </div>
+            </main>
+        </div>
     </div>
 </div>
 @endsection
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,7 +1,31 @@
-<div>
-    <div class="container mx-auto mt-4" x-data="{tab:0}">
-        <div class="grid grid-cols-1 px-4 mb-20 sm:px-6 xl:mb-0">
-        <p>HOME</p>
+<div class="flex flex-col h-[80vh] space-y-6 overflow-hidden">
+    <div class="flex flex-col space-y-4 h-[40%]">
+        <div class="grid grid-cols-3 gap-4 h-full">
+            <!-- Stats Section -->
+            <div class="col-span-2 flex">
+                <div class="grid grid-cols-4 gap-4 w-full">
+                    <x-card>stat 1</x-card>
+                    <x-card>stat 2</x-card>
+                    <x-card>stat 3</x-card>
+                    <x-card>stat 4</x-card>
+                </div>
+            </div>
+            <!-- Bar Chart Section -->
+            <div class="flex w-full">
+                <x-card class="w-full">bar chart</x-card>
+            </div>
+        </div>
+    </div>
+    <div class="flex flex-col space-y-4 h-[60%]">
+        <div class="grid grid-cols-3 gap-4 h-full">
+            <!-- Line Chart Section -->
+            <div class="col-span-2 flex">
+                <x-card class="w-full">line chart</x-card>
+            </div>
+            <!-- Pie Chart Section -->
+            <div class="flex w-full">
+                <x-card class="w-full">pie chart</x-card>
+            </div>
         </div>
     </div>
 </div>

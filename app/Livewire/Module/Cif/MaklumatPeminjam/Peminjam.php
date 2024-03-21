1fa28a6b53b5cccfe71c82afc80c5346a9a8ddf9
Nazirul Fitri <nazirul@csc.net.my>
2024-03-21 10:34:49 +0800

    CIF Peminjam

---
+++ app/Livewire/Module/Cif/MaklumatPeminjam/Peminjam.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Module\Cif\MaklumatPeminjam;
+
+use Livewire\Component;
+
+class Peminjam extends Component
+{
+    public function render()
+    {
+        return view('livewire.module.cif.maklumat-peminjam.peminjam');
+    }
+}
---
+++ resources/views/livewire/module/cif/maklumat-peminjam/peminjam.blade.php
@@ -0,0 +1,21 @@
+<x-card>
+    <div class="p-4">
+        <x-input label="Nama Peminjam" disabled />
+        <div class="grid grid-cols-3 gap-4 mt-4">
+            <x-input label="Kad Pengenalan Baru" disabled />
+            <x-input label="Kad Pengenalan Lama" disabled />
+            <x-input label="Status Bumiputra" disabled />
+            <x-input label="Bahasa Pilihan" disabled />
+            <x-input label="Negeri" disabled />
+            <x-input label="Kawasan" disabled />
+            <x-input label="Umur" disabled />
+            <x-input label="Agama" disabled />
+            <x-input label="Bangsa / Kaum" disabled />
+            <x-input label="Kategori Khas (OKU)" disabled />
+            <x-input label="Suku Kaum" disabled />
+        </div>
+        <div class="mt-4">
+            <x-input label="Nama Syarikat" disabled />
+        </div>
+    </div>
+</x-card>
